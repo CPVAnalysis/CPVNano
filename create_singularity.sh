137c479f6eda86761673a8758ca4903c795d364b
anlyon <anne-mazarine.lyon@cern.ch>
2025-02-25 08:27:02 +0100

    create_singularity.sh added

---
+++ create_singularity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The Run 2 releases (10_2_X and 10_6_X) operate on SLC7 architecture, while 
+# lxplus has EL8 or EL9 architectures
+
+# To be able to setup the environment, create a singularity compatible with SLC7
+
+# Info on singularity: https://cms-sw.github.io/singularity.html
+
+cmssw-el7
