4d08ff03eeb6912dfd2cc5d9335820093dc29441
anlyon <anne-mazarine.lyon@cern.ch>
2025-07-29 20:17:18 +0200

    samples added

---
+++ CPVNano/data/samples/bparkingdata_samples_2018.py
@@ -0,0 +1,28 @@
+bpark_samples_2018 = {}
+
+bpark_samples_2018['A1'] = '/ParkingBPH1/Run2018A-05May2019-v1/MINIAOD'
+bpark_samples_2018['A2'] = '/ParkingBPH2/Run2018A-05May2019-v1/MINIAOD'
+bpark_samples_2018['A3'] = '/ParkingBPH3/Run2018A-05May2019-v1/MINIAOD'
+bpark_samples_2018['A4'] = '/ParkingBPH4/Run2018A-05May2019-v1/MINIAOD'
+bpark_samples_2018['A5'] = '/ParkingBPH5/Run2018A-05May2019-v1/MINIAOD'
+bpark_samples_2018['A6'] = '/ParkingBPH6/Run2018A-05May2019-v1/MINIAOD'
+
+bpark_samples_2018['B1'] = '/ParkingBPH1/Run2018B-05May2019-v2/MINIAOD'
+bpark_samples_2018['B2'] = '/ParkingBPH2/Run2018B-05May2019-v2/MINIAOD'
+bpark_samples_2018['B3'] = '/ParkingBPH3/Run2018B-05May2019-v2/MINIAOD'
+bpark_samples_2018['B4'] = '/ParkingBPH4/Run2018B-05May2019-v2/MINIAOD'
+bpark_samples_2018['B5'] = '/ParkingBPH5/Run2018B-05May2019-v2/MINIAOD'
+bpark_samples_2018['B6'] = '/ParkingBPH6/Run2018B-05May2019-v2/MINIAOD'
+
+bpark_samples_2018['C1'] = '/ParkingBPH1/Run2018C-05May2019-v1/MINIAOD' 
+bpark_samples_2018['C2'] = '/ParkingBPH2/Run2018C-05May2019-v1/MINIAOD' 
+bpark_samples_2018['C3'] = '/ParkingBPH3/Run2018C-05May2019-v1/MINIAOD' 
+bpark_samples_2018['C4'] = '/ParkingBPH4/Run2018C-05May2019-v1/MINIAOD' 
+bpark_samples_2018['C5'] = '/ParkingBPH5/Run2018C-05May2019-v1/MINIAOD' 
+
+bpark_samples_2018['D1'] = '/ParkingBPH1/Run2018D-05May2019promptD-v1/MINIAOD'
+bpark_samples_2018['D2'] = '/ParkingBPH2/Run2018D-05May2019promptD-v1/MINIAOD'
+bpark_samples_2018['D3'] = '/ParkingBPH3/Run2018D-05May2019promptD-v1/MINIAOD'
+bpark_samples_2018['D4'] = '/ParkingBPH4/Run2018D-05May2019promptD-v1/MINIAOD'
+bpark_samples_2018['D5'] = '/ParkingBPH5/Run2018D-05May2019promptD-v1/MINIAOD'
+
---
+++ CPVNano/data/samples/bparkingdata_samples_2022.py
@@ -0,0 +1,22 @@
+bpark_samples_2022 = {}
+
+bpark_samples_2022['C0'] = '/ParkingSingleMuon0/Run2022C-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D0'] = '/ParkingSingleMuon0/Run2022D-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D0_v2'] = '/ParkingSingleMuon0/Run2022D-PromptReco-v2/MINIAOD'
+bpark_samples_2022['E0'] = '/ParkingSingleMuon0/Run2022E-PromptReco-v1/MINIAOD'
+bpark_samples_2022['F0'] = '/ParkingSingleMuon0/Run2022F-PromptReco-v1/MINIAOD'
+bpark_samples_2022['G0'] = '/ParkingSingleMuon0/Run2022G-PromptReco-v1/MINIAOD'
+
+bpark_samples_2022['C1'] = '/ParkingSingleMuon1/Run2022C-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D1'] = '/ParkingSingleMuon1/Run2022D-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D1_v2'] = '/ParkingSingleMuon1/Run2022D-PromptReco-v2/MINIAOD'
+bpark_samples_2022['E1'] = '/ParkingSingleMuon1/Run2022E-PromptReco-v1/MINIAOD'
+bpark_samples_2022['F1'] = '/ParkingSingleMuon1/Run2022F-PromptReco-v1/MINIAOD'
+bpark_samples_2022['G1'] = '/ParkingSingleMuon1/Run2022G-PromptReco-v1/MINIAOD'
+
+bpark_samples_2022['C2'] = '/ParkingSingleMuon2/Run2022C-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D2'] = '/ParkingSingleMuon2/Run2022D-PromptReco-v1/MINIAOD'
+bpark_samples_2022['D2_v2'] = '/ParkingSingleMuon2/Run2022D-PromptReco-v2/MINIAOD'
+bpark_samples_2022['E2'] = '/ParkingSingleMuon2/Run2022E-PromptReco-v1/MINIAOD'
+bpark_samples_2022['F2'] = '/ParkingSingleMuon2/Run2022F-PromptReco-v1/MINIAOD'
+bpark_samples_2022['G2'] = '/ParkingSingleMuon2/Run2022G-PromptReco-v1/MINIAOD'
